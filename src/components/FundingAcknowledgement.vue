7af8fc503781a85829acc8c591ff570cd50124a0
Carifio24 <carifio24@gmail.com>
2024-04-12 18:31:46 -0400

    Add flexibility to funding acknowledgement component.

--- src/components/FundingAcknowledgement.vue
+++ src/components/FundingAcknowledgement.vue
@@ -1,13 +1,36 @@
 <template>
-  <div id="funding-acknowledgment">
+  <div
+    id="funding-acknowledgment"
+    :style="cssVars"
+  >
     The material contained in this product is based upon work supported by NASA under cooperative agreement award No. 80NSSC21M0002. Any opinions, findings, and conclusions or recommendations expressed in this material are those of the authors and do not necessarily reflect the views of the National Aeronautics and Space Administration.
   </div>
 </template>
 
-<style lang="less">
+<script setup lang="ts">
+import { computed } from "vue";
+export interface FundingAcknowledgementProps {
+  color?: string;
+  backgroundColor?: string;
+}
+
+const props = withDefaults(defineProps<FundingAcknowledgementProps>(), {
+  color: "#E0E0E0",
+  backgroundColor: "#0C3D91",
+});
+
+const cssVars = computed(() => {
+  return {
+    "--color": props.color,
+    "--background-color": props.backgroundColor,
+  };
+});
+</script>
+
+<style scoped lang="less">
 #funding-acknowledgment {
-  color: #E0E0E0;
-  background-color: #0c3d91;
+  color: var(--color);
+  background-color: var(--background-color);
   font-size: calc(0.8em + 0.1vw + 0.1vh);
   line-height: calc(1em + 0.3vw + 0.3vh);
   padding-inline: 1em;
