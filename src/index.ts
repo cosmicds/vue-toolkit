ca0dcd86dee5f0f9507a05926105461ea2aa6db6
Carifio24 <carifio24@gmail.com>
2024-04-18 17:39:51 -0400

    Reorganize index file.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
+import { BackgroundImageset, skyBackgroundImagesets } from "./background";
+import { useFullscreen, useWindowShape, WindowShape } from "./composables";
+import { PositionCoords, useGeolocation } from "./geolocation";
 import { KeyboardControlSettings } from "./keyboard";
+import { API_BASE_URL, MINIDS_BASE_URL, R2D, D2R, supportsTouchscreen, isMobile, blurActiveElement, filterInPlace } from "./utils";
+import { useWWTKeyboardControls } from "./wwt-utils";
+
 import IconButton from "./components/IconButton.vue";
 import LocationSelector from "./components/LocationSelector.vue";
 import FundingAcknowledgement from "./components/FundingAcknowledgement.vue";
@@ -6,11 +12,6 @@ import CreditLogos from "./components/CreditLogos.vue";
 import GeolocationButton from "./components/GeolocationButton.vue";
 import wwtHUD from "./components/wwtHUD.vue";
 import Gallery from "./components/Gallery.vue";
-import { BackgroundImageset, skyBackgroundImagesets } from "./background";
-import { useFullscreen, useWindowShape, WindowShape } from "./composables";
-import { PositionCoords, useGeolocation } from "./geolocation";
-import { useWWTKeyboardControls } from "./wwt-utils";
-import { API_BASE_URL, MINIDS_BASE_URL, R2D, D2R, supportsTouchscreen, isMobile, blurActiveElement, filterInPlace } from "./utils";
 
 export {
   BackgroundImageset,
