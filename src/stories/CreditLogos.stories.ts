be670c6744134a31efb5473e9a6b49a582ec7b59
Carifio24 <carifio24@gmail.com>
2024-04-28 11:11:33 -0400

    Component will now display in Storybook, but CSS is not being used.

---
+++ src/stories/CreditLogos.stories.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+import { Meta, StoryObj } from "@storybook/vue3";
+import { CreditLogosProps } from "..";
+
+import CreditLogos from "../components/CreditLogos.vue";
+
+const meta: Meta<typeof CreditLogos> = {
+  component: CreditLogos,
+};
+
+export default meta;
+type Story = StoryObj<typeof CreditLogos>;
+
+export const Primary: Story = {
+  render: (args: CreditLogosProps) => ({
+    components: { CreditLogos },
+    template: `<CreditLogos v-bind="args" />`,
+    setup() {
+      return { args };
+    },
+    args: {
+      "--logo-size": "5vmin" 
+    }
+  })
+};
