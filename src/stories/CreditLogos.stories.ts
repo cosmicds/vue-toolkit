c383e9a8ca71c37e2b15c988d63555553c78d283
Carifio24 <carifio24@gmail.com>
2024-05-29 15:28:14 -0400

    Add icon button story and update credit logos story.

--- src/stories/CreditLogos.stories.ts
+++ src/stories/CreditLogos.stories.ts
@@ -20,6 +20,14 @@ export const Primary: Story = {
     }
   }),
   args: {
-    logoSize: "5vmin"
+    logoSize: "5vmin",
+    extraLogos: [
+      {
+        alt: "Smithsonian Logo",
+        src: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Smithsonian_sun_logo_no_text.svg/1024px-Smithsonian_sun_logo_no_text.svg.png",
+        href: "https://www.si.edu/"
+      }
+    ],
+    defaultLogos: ["wwt", "cosmicds", "nasa", "sciact"],
   }
 };
---
+++ src/stories/IconButton.stories.ts
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+import { Meta, StoryObj } from "@storybook/vue3";
+import { IconButtonProps } from "../types";
+import IconButton from "../components/IconButton.vue";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBookOpen } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faBookOpen);
+
+const meta: Meta<typeof IconButton> = {
+  component: IconButton,
+};
+
+export default meta;
+type Story = StoryObj<typeof IconButton>;
+
+export const Primary: Story = {
+  render: (args: IconButtonProps) => ({
+    components: { IconButton },
+    template: `<IconButton v-bind="args" />`,
+    setup() {
+      return { args };
+    }
+  }),
+  decorators: [
+    () => {
+      return { template: `<div style="width: 50px"><story /></div>` };
+    }
+  ],
+  args: {
+    modelValue: false,
+    faIcon: "book-open",
+    mdIcon: null,
+    color: "white",
+    focusColor: "red",
+    backgroundColor: "#040404",
+    boxShadow: true,
+    border: true,
+    longPressTimeMs: 500,
+    tooltipText: "Tooltip",
+    tooltipLocation: "start",
+    tooltipOnClick: false,
+    tooltipOnFocus: false,
+    tooltipOnHover: true,
+    showTooltip: true,
+    faSize: "lg",
+    mdSize: "100px"
+  }
+};
