6ce3b4cc687c3bf8db60d502f873c0dac6307b59
Carifio24 <carifio24@gmail.com>
2024-05-29 15:27:29 -0400

    Add Vuetify plugin to Storybook.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,17 @@
-import type { Preview } from "@storybook/vue3";
+import { setup, type Preview } from "@storybook/vue3";
+
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+  components,
+  directives
+});
+
+setup((app) => {
+  app.use(vuetify);
+});
 
 const preview: Preview = {
   parameters: {
