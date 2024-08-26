b6355a551fc9a8c509cab7c81978e39033c27ff3
Carifio24 <carifio24@gmail.com>
2024-08-26 13:20:28 -0400

    Set up gallery story. Start setting up WWT HUD story.

---
+++ src/stories/WwtHud.stories.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+import { Meta, StoryObj } from "@storybook/vue3";
+import { WwtHud, WwtHudProps } from "..";
+
+const meta: Meta<typeof WwtHud> = {
+  component: WwtHud,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof WwtHud>;
