4e5368db7441e92770bcdf0933bef57de711b0db
Carifio24 <carifio24@gmail.com>
2025-02-24 09:55:39 -0500

    Add CSS to use across stories, along with basic story for speed controls.

---
+++ src/stories/SpeedControl.stories.ts
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+import { Meta, StoryObj } from "@storybook/vue3";
+import { engineStore, WWTComponent } from "@wwtelescope/engine-pinia";
+import { SpeedControlProps, SpeedControl } from "..";
+
+import "./stories.css";
+
+const meta: Meta<typeof SpeedControl> = {
+  component: SpeedControl,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof SpeedControl>;
+
+export const Primary: Story = {
+  render: (args: SpeedControlProps) => {
+    const store = engineStore();
+    return {
+      components: { SpeedControl, WWTComponent },
+      template: `
+        <div style="width: 700px; height: 400px">
+          <WWTComponent
+            :wwtNamespace="storybook"
+          />
+          <SpeedControl v-bind="args" :store="store" />
+        </div>
+      `,
+      setup() {
+        return { args, store };
+      }
+    };
+  },
+  args: {
+    color: "white",
+    maxSpeed: 10000,
+    defaultRate: 10,
+    useInline: true,
+    showStatus: false,
+    rateDelta: 10,
+    startPlaying: true,
+  }
+};
