5bb56e2e69b2a99494102aee6a022dabd982ac11
Carifio24 <carifio24@gmail.com>
2025-06-12 17:29:38 -0400

    Create opacity controls story component.

---
+++ src/stories/docs/OpacityControls.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Image Opacity Controls</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+/* eslint-disable indent */
+
+import { ref } from "vue";
+import CodeBlock from "./CodeBlock.vue";
+</script>
