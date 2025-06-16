8b8a4b71a30ceb0078d2a3529b91a2b63ace1b4f
Carifio24 <carifio24@gmail.com>
2025-06-16 10:53:51 -0400

    Almost done with opacity controls guide.

--- src/stories/docs/OpacityControls.vue
+++ src/stories/docs/OpacityControls.vue
@@ -23,11 +23,76 @@
       </ul>
     </p>
     <p>
-      Let's implement these in order, so we'll start with the button to show the JWST image only. First, let's add a container for our control widgets, above the credit logos:
+      Additionally, we'll want these UI elements to be in sync - for example, if a user presses the JWST button, the slider should move all the way to the JWST end 
+      as well. To help facilitate that, let's create a reactive variable that will let Vue help us with that. There's no one right way to set this up, but for this 
+      story, let's define a variable called <code>crossfade</code> that goes from 0 to 100, with 0 meaning that only the Hubble image is visible, and 100 meaning 
+      that only the JWST image is visible. Add the following at the top level of the component:
+    </p>
+    <CodeBlock code="const crossfade = ref(50);" lang="javascript" />
+    <p>
+      And then let's have Vue hook this variable up to our imageset layer opacities. Also at the top level of the component, add:
+    </p>
+    <CodeBlock :code="crossfadeWatcher" lang="javascript" />
+    <p>And update the first line of the script section of the template to be</p>
+    <CodeBlock :code="importWithWatch" lang="javascript" />
+    <p>
+      This sets up a <a href="https://vuejs.org/guide/essentials/watchers.html" target="_blank" rel="noopener noreferrer">watcher</a>, which is a piece of code 
+      that Vue will automatically run whenever the value of the specified reactive variable (here <code>crossfade</code>) is updated. What this really means is that now
+      we can entirely control the opacities of our imagesets through <code>crossfade</code>, and they'll automatically have the complementary behavior that we want.
+    </p>
+    <p>
+      Now that we have a reactive way to control our opacities, let's set up our control widgets. First, let's add a container for them, above the credit logos:
     </p>
     <CodeBlock :code="controlsContainer" lang="html" />
-    <p>The bottom content container is already set up as a flex container with <code>flex-direction: column</code>, so the controls will automatically show up above the logos 
-    (if this doesn't mean anything to you, it's just a fancy way of saying the that bottom content container will vertically stack its top-level items
+    <p>
+      The bottom content container is already set up as a flex container with <code>flex-direction: column</code>, so the controls will automatically show up above the logos 
+      (if this doesn't mean anything to you, it's just a fancy way of saying the that bottom content container will vertically stack its top-level items).
+    </p>
+    <p>
+      Now that we've got a container, let's add our first button - a button to set the current view to show only the Hubble image. There are a look of ways that we could 
+      create a button - the HTML button element, Vuetify's <code>v-btn</code> component, etc., but to allow us to do some really simple styling here we'll use a 
+      <code>span</code> element with a click listener. Inside the <code>controls-container</code>, add the following:
+    </p>
+    <CodeBlock :code="hubbleButton" lang="html" />
+    <p>
+      Breaking this down a bit: <code>@click="crossfade = 0"</code> tells Vue to set our <code>crossfade</code> value to zero when the button is clicked.
+      <code>@keyup.enter="crossfade = 0"</code> does the same thing when the Enter button is pressed when the button is focused. 
+      The line <code>tabindex="0"</code> allows the button to be tab-focusable. Together these help make the controls more accessible 
+      for users who are navigating via the keyboard.
+    </p>
+    <v-alert type="info" variant="tonal">
+      <template #text>
+        Note that when we update Vue refs in the JavaScript portion of the template, we need to update the underlying value by setting the <code>value</code> attribute 
+        of the ref, but in the template this isn't necessary. Internally, Vue will automatically translate <code>crossfade = 0</code> in the template into 
+        <code>crossfade.value = 0</code>.
+      </template>
+    </v-alert>
+    <p>We can do almost the same thing for the JWST button:</p>
+    <CodeBlock :code="jwstButton" lang="html" />
+    <p>The functionality is the same here, except now we're setting <code>crossfade = 100</code>.</p>
+    <p>
+      Finally, let's add the slider to allow users to adjust the crossfade between the two images. For this, we'll use Vuetify's slider component. Add the following 
+      to the template between the markup for the two buttons:
+    </p>
+    <CodeBlock :code="crossfadeSlider" lang="html" />
+    <p>
+      The markup for the slider is pretty straightforward. <code>min</code> and <code>max</code> are the min and max values of the slider, and <code>step</code> is the 
+      gap between adjacent slider values. The <code>v-model="crossfade"</code> line hooks up the value of the slider to our crossfade value. This means that changing the 
+      value of the slider will adjust the value of <code>crossfade</code>, and changing the value of crossfade somewhere else will cause the slider to update to match.
+    </p>
+    <p>
+      Finally, let's set up the layout for the controls. To keep things simple, let's just have the controls container lay its items out in a single row. Add the following in 
+      the style section of the template:
+    </p>
+    <CodeBlock :code="controlsContainerCSS" lang="css" />
+    <p>The main highlights of this are:
+      <ul>
+        <li>Setting <code>display: flex</code> and <code>flex-direction: row</code> will cause the container to lay its items out in a single row</li>
+        <li>
+          In order to not block the pointer from adjusting the WWT view, we have things in the interactive set to <code>pointer-events: none</code> by default. 
+          So in order for these to be usable, we need to be <code>pointer-events: auto</code> for the controls container.
+        </li>
+      </ul>
     </p>
   </div>
 </template>
@@ -49,11 +114,59 @@ const bottomContentEmpty = ref(
 
 const controlsContainer = ref(
 `<div id="bottom-content">
-  <div id="controls-container">
-  </div>
-  <div id="body-logos" v-if= "!smallSize">
-    <credit-logos/>
-  </div>
+   <div id="controls-container">
+   </div>
+   <div id="body-logos" v-if= "!smallSize">
+     <credit-logos/>
+   </div>
 </div>
 `);
+
+const crossfadeWatcher = ref(
+`watch(crossfade, (value: number) => {
+   const jwstOpacity = 0.01 * value;
+   layers.jwst.set_opacity(jwstOpacity);
+   layers.hubble.set_opacity(1 - jwstOpacity);
+});
+`);
+
+const importWithWatch = ref(`import { ref, reactive, computed, onMounted, nextTick } from "vue";`);
+
+const hubbleButton = ref(
+`<span
+  class="ui-text slider-label"
+  @click="crossfade = 0"
+  @keyup.enter="crossfade = 0"
+  tabindex="0"
+>Hubble<br><span class="light-type">(Visible)</span></span>
+`);
+
+const jwstButton = ref(
+`<span
+  class="ui-text slider-label"
+  @click="crossfade = 100"
+  @keyup.enter="crossfade = 100"
+  tabindex="0"
+>JWST<br><span class="light-type">(Infrared)</span></span>
+`);
+
+const crossfadeSlider = ref(
+`<v-slider
+  v-model="crossfade"
+  :min="0"
+  :max="100"
+  :step="1"
+></v-slider>
+`);
+
+const controlsContainerCSS = ref(
+`#controls-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 5px;
+  pointer-events: auto;
+}
+`);
+
 </script>
