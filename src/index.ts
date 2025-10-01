f51f97262cbdb83aa92694d84fda3339f912967d
Carifio24 <carifio24@gmail.com>
2025-10-01 11:43:47 -0400

    Export user experience stuff.

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,11 @@ import LocationSelector from "./components/LocationSelector.vue";
 import DefaultMiniCredits from "./components/DefaultMiniCredits.vue";
 import FundingAcknowledgment from "./components/FundingAcknowledgment.vue";
 import CreditLogos from "./components/CreditLogos.vue";
+import UserExperience from "./components/UserExperience.vue";
 import wwtHUD from "./components/wwtHUD.vue";
 import { BackgroundImageset, skyBackgroundImagesets } from "./background";
 import Gallery from "./components/Gallery.vue";
-import { API_BASE_URL, MINIDS_BASE_URL, R2D, D2R } from "./utils";
+import { API_BASE_URL, MINIDS_BASE_URL, R2D, D2R, UserExperienceRating, DEFAULT_RATING_COLORS, submitUserExperienceRating, UserExperienceSubmissionInfo } from "./utils";
 
 export {
   BackgroundImageset,
@@ -21,9 +22,14 @@ export {
   DefaultMiniCredits,
   FundingAcknowledgment,
   CreditLogos,
+  UserExperience,
   wwtHUD,
   API_BASE_URL,
   MINIDS_BASE_URL,
   R2D,
-  D2R
+  D2R,
+  UserExperienceRating,
+  DEFAULT_RATING_COLORS,
+  submitUserExperienceRating,
+  UserExperienceSubmissionInfo,
 };
