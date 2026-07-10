c7f2c81505a3b0192b7ecc3fc8e37f8c5b4276c4
Jonathan Carifio <carifio24@gmail.com>
2026-07-10 13:13:51 -0400

    Add button role to the icon button.

--- src/components/IconButton.vue
+++ src/components/IconButton.vue
@@ -22,6 +22,7 @@
         tabindex="0"
         :aria-disabled="disabled"
         :aria-label="ariaLabel"
+        role="button"
       >
         <slot name="button">
           <font-awesome-icon
