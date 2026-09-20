059da19e2464864e8ebd6e2bb846fd4630457b89
Carifio24 <carifio24@gmail.com>
2026-09-20 11:47:27 -0400

    Update icon button story test. Tweak component disabled functionality.

--- src/stories/IconButton.stories.ts
+++ src/stories/IconButton.stories.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
 import { Meta, StoryObj } from "@storybook/vue3-vite";
+import { expect, userEvent, waitFor, within } from "@storybook/test";
 import { IconButtonProps } from "../types";
 import { IconButton } from "..";
 
@@ -8,6 +9,7 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faBookOpen } from "@fortawesome/free-solid-svg-icons";
 
 import "./stories.css";
+import { styleColor } from "./utils";
 
 library.add(faBookOpen);
 
@@ -51,5 +53,44 @@ export const Primary: Story = {
     showTooltip: true,
     size: "lg",
     disabled: false,
-  }
+  },
+  play: async ({ args, canvasElement }) => {
+    args.disabled = false;
+    const canvas = within(canvasElement);
+    const button = canvas.getByRole("button");
+    await userEvent.hover(button);
+    const tooltip = document.querySelector(".v-tooltip");
+    const tooltipContent = tooltip.querySelector(".v-overlay__content");
+    let buttonStyle = window.getComputedStyle(button);
+    expect(tooltipContent).toBeVisible();
+    expect(tooltipContent).toHaveTextContent(args.tooltipText);
+
+    await userEvent.unhover(button);
+    await waitFor(() => {
+      expect(tooltipContent).not.toBeVisible();
+    });
+
+    await userEvent.click(button);
+    buttonStyle = window.getComputedStyle(button);
+    expect(buttonStyle.color).toBe(styleColor(args.focusColor));
+
+    args.modelValue = true;
+    await waitFor(() => {
+      buttonStyle = window.getComputedStyle(button);
+      expect(buttonStyle.color).toBe(styleColor(args.activeColor));
+      args.modelValue = false;
+    });
+
+    args.disabled = true;
+    await waitFor(() => {
+      buttonStyle = window.getComputedStyle(button);
+      expect(buttonStyle.cursor).toBe("not-allowed");
+    });
+
+    args.disabled = true;
+    await waitFor(() => {
+      buttonStyle = window.getComputedStyle(button);
+      expect(buttonStyle.cursor).toBe("default");
+    });
+  },
 };
--- src/stories/utils.ts
+++ src/stories/utils.ts
@@ -8,3 +8,22 @@ export const descriptionDecorator: Decorator = (_story, parameters) => ({
     </div>
   `
 });
+
+/**
+ * This uses the browser to generate the color string.
+ * This should generally be an rgb color string, but this way
+ * requires zero maintenance as we don't need to worry about what
+ * the browser is going to do.
+ */
+export function styleColor(color: string): string {
+  const div = document.createElement("div");
+  div.style.color = color;
+  document.body.appendChild(div);
+  const rgb = window.getComputedStyle(div).color;
+  div.remove();
+  return rgb;
+}
+
+export async function sleep(ms: number): Promise<void> {
+  return new Promise((resolve, _reject) => setTimeout(resolve, ms));
+}
